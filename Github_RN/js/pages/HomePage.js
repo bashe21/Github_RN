07a6136692492e5998fbea7d4242d9430984cc0c
zyx21 <3064662835@qq.com>
2020-06-28 18:28:22 +0800

    [feat]添加页面和导航文件

---
+++ Github_RN/js/pages/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default class WelcomePage extends React.Component {
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            // 跳转到首页
+
+        }, 200);
+    }
+
+    componentWillUnmount() {
+        // 页面销毁时，清空计时器
+        this.timer && clearTimeout(this.timer);
+    }
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Welcome to you!</Text>
+            </View>
+        )
+    }
+}
