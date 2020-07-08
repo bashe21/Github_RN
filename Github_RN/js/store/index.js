bc698c1a7eb0e98be993caa0077c1f62e4b1d0b4
zyx21 <3064662835@qq.com>
2020-07-08 21:11:16 +0800

    [feat]添加中间件

--- Github_RN/js/store/index.js
+++ Github_RN/js/store/index.js
@@ -2,8 +2,20 @@ import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducer'
 
+const logger = store => next => action => {
+    if (typeof action === 'function') {
+        console.log('dispatching a function');
+    } else {
+        console.log('dispatching', action);
+    }
+    const result = next(action);
+    console.log('nextState', store.getState());
+    return result;
+};
+
 const middlewares = [
-    thunk
+    thunk,
+    logger,
 ];
 
 // 创建store
