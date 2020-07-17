93feda26e928c8e4104c912cf75963dad370ac04
zyx21 <3064662835@qq.com>
2020-07-17 16:58:49 +0800

    [feat]我的界面搭建

--- Github_RN/js/pages/MyPage.js
+++ Github_RN/js/pages/MyPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, ScrollView} from 'react-native';
 import Featcher from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import NavigatorBar from '../public/NavigatorBar';
+import {MoreMenu} from '../public/MoreMenu';
+import GlobalStyles from '../res/GlobalStyles';
+import ViewUtils from '../util/ViewUtils';
+import { color } from 'react-native-reanimated';
 
 const THEME_COLOR = '#678';
 
@@ -10,6 +14,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1, 
         // marginTop: 32,
+    },
+    about_left: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+    groupTitle: {
+        marginLeft: 10, 
+        marginTop: 10, 
+        marginBottom: 5, 
+        fontSize: 12, 
+        color: 'gray',
     }
 })
 
@@ -44,6 +67,14 @@ export default class MyPage extends React.Component {
             </TouchableOpacity>
         )
     }
+
+    onClick(menu) {
+
+    }
+
+    getItem(menu) {
+        return ViewUtils.getMenuItem(() => this.onClick(menu), menu, THEME_COLOR);
+    }
     
     render() {
         let statusBar = {
@@ -60,9 +91,69 @@ export default class MyPage extends React.Component {
         />
         
         return (
-            <View style={styles.container}>
+            <View style={GlobalStyles.root_container}>
                 {navigationBar}
-                <Text>MyPage!</Text>
+                <ScrollView>
+                    <TouchableOpacity
+                        onPress = {() => this.onClick(MoreMenu.About)}
+                        style = {styles.item}
+                    >
+                        <View style={styles.about_left}>
+                            <Ionicons 
+                                name={MoreMenu.About.icon}
+                                size={40}
+                                style={{
+                                    marginRight: 10,
+                                    color: THEME_COLOR,
+                                }}
+                            />
+                            <Text>Github Popular</Text>
+                        </View>
+                        <Ionicons
+                            name={'ios-arrow-forward'}
+                            size={16}
+                            style={{
+                                marginLeft: 10,
+                                alignSelf: 'center',
+                                color: THEME_COLOR,
+                            }}
+                        />
+                        
+                    </TouchableOpacity>
+                    <View style={GlobalStyles.line}/>
+
+                    {this.getItem(MoreMenu.Tutorial)}
+                    {/* {趋势管理} */}
+                    <Text style = {styles.groupTitle}>趋势管理</Text>
+                    {/* {自定义语言} */}
+                    {this.getItem(MoreMenu.Custom_Language)}
+                    <View style={GlobalStyles.line}/>
+                    {/* {语言排序} */}
+                    {this.getItem(MoreMenu.Sort_Language)}
+
+                    {/* {最热管理} */}
+                    <Text style = {styles.groupTitle}>最热管理</Text>
+                    {/* {自定义标签} */}
+                    {this.getItem(MoreMenu.Custom_Key)}
+                    {/* {标签排序} */}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MoreMenu.Sort_Key)}
+                    {/* {标签移除} */}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MoreMenu.Remove_Key)}
+
+                    {/* {设置管理} */}
+                    <Text style = {styles.groupTitle}>设置</Text>
+                    {/* {自定义主题} */}
+                    {this.getItem(MoreMenu.Custom_Theme)}
+                    {/* {关于作者} */}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MoreMenu.About_Author)}
+                    {/* {反馈} */}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MoreMenu.Feedback)}
+
+                </ScrollView>
             </View>
         )
     }
--- Github_RN/js/util/ViewUtils.js
+++ Github_RN/js/util/ViewUtils.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TouchableOpacity, StyleSheet, View, Text} from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { exp } from 'react-native-reanimated';
 
 
 export default class ViewUtils {
@@ -37,4 +38,71 @@ export default class ViewUtils {
 
         </TouchableOpacity>
     }
+
+    /* 
+    获取设置页的Item
+    @params callback 单击item的回调
+    @params text 显示的文本
+    @params color 图标着色
+    @params Icons react-native-vector-icons组件
+    @params icon 左侧图标
+    @params expandableIcon 右侧图片
+    return {XML}
+    
+    */
+    static getSettingItem(callback, text, color, Icons, icon, expandableIcon) {
+        return (
+            <TouchableOpacity
+                onPress = {callback}
+                style = {styles.setting_item_container}
+            >
+                <View style={{alignItems: 'center', flexDirection: 'row'}}>
+                    {
+                        (Icons && icon) ? (
+                            <Icons 
+                            name={icon}
+                            size={16}
+                            style={{color: color, marginRight: 10}}/>
+                        ) : 
+                        <View style = {{opacity: 1, width: 16, height: 16, marginRight: 10,}}/>  
+                    }
+                    <Text>{text}</Text>
+                </View>
+                <Ionicons
+                    name={expandableIcon ? expandableIcon : 'ios-arrow-forward'}
+                    size={16}
+                    style={{
+                        marginLeft: 10,
+                        alignSelf: 'center',
+                        color: color || 'black',
+                    }}
+                />
+            </TouchableOpacity>
+        )
+    }
+
+
+    /* 
+    获取设置页面的Item
+    @params callback 单击item的回调
+    @params menu @MoreMenu
+    @parmams color 图标着色
+    @params expandableIcon 右侧图标
+    @return {XML}
+    */
+    static getMenuItem(callback, menu, color, expandableIcon) {
+        return ViewUtils.getSettingItem(callback, menu.name, color, menu.Icons, menu.icon, expandableIcon);
+    }
 }
+
+
+const styles = StyleSheet.create({
+    setting_item_container: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    }
+})
